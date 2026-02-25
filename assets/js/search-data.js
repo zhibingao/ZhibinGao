8a2499bb90b2e0e9e7342b7f1a084fb16b82bded
zhibingao <zhibingao@outlook.com>
2026-02-25 08:39:28 +0000

    Deploying to gh-pages from @ zhibingao/zhibingao.github.io@12bdfc64c49d0480a2dab1c71f021aea2cead4d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,16 +35,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-恭喜刘宇杰获得2024年中国移动创客马拉松大赛三等奖",
+          title: '恭喜刘宇杰获得2024年中国移动创客马拉松大赛三等奖',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
