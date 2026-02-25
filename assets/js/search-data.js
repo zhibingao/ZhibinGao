4d713f222eba9b373805b2249dc6454974bb3514
zhibingao <zhibingao@outlook.com>
2026-02-25 09:55:20 +0000

    Deploying to gh-pages from @ zhibingao/zhibingao.github.io@13bd7ddb71a5e903e3340a1b8c2af94c4767874a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
